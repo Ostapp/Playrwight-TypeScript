094fce5998cfc09c74714454c99dcf9c67d9451e
Ostapp <ostap.didenko@gmail.com>
2023-10-20 12:52:10 +0100

    removed amazon test

--- tests/amazon.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.amazon.com/ebike/s?k=ebike');
-  const links = await page.$$('h2 a.a-link-normal');  
-  links[0].click();
-});
